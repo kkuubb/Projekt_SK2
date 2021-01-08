02470bb0ac8bce6b9e8bac98f55c8ffb8741cc51
Asia <joanna.brnk@gmail.com>
2021-01-08 12:18:13 +0100

    add server pattern

--- serwer.cpp
+++ serwer.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
@@ -12,46 +11,97 @@
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
-#include <vector>
-#include <unordered_map>
-#include <stdlib.h>
-#include <algorithm>
-#include <signal.h>
 
-//bool wlaczony = false;
-struct sockaddr_in serv;
-int fd;
-int conn;
-char message[100] = "";
-struct addrinfo address, *res, *p;
+#define SERVER_PORT 1234
+#define QUEUE_SIZE 5
 
-void inicjalizacjaSerwera(char* ipAddres, char* port)
+//struktura zawierajÄca dane, ktĂłre zostanÄ przekazane do wÄtku
+struct thread_data_t
 {
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons((uint16_t)port);
-    serv.sin_addr.s_addr = INADDR_ANY;
-    if (fd = socket(AF_INET, SOCK_STREAM, 0) > 0)
-    {
-        puts("Cos poszlo nie tak");
-    }
-    bind(fd, (struct sockaddr *)&serv, sizeof(serv));
-    listen(fd, 5);
-    while(conn = accept(fd, (struct sockaddr *)NULL, NULL))
-    {
-        int pid;
-        if((pid = fork()) == 0)
-            while (recv(conn, message, 100, 0)>0) 
-            {
-            printf("Message Received: %s\n", message);
-            char message[100] = "";
-            }
-            exit(0);
+//TODO
+};
+
+//funkcja opisujÄcÄ zachowanie wÄtku - musi przyjmowaÄ argument typu (void *) i zwracaÄ (void *)
+void *ThreadBehavior(void *t_data)
+{
+    pthread_detach(pthread_self());
+    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
+    //dostÄp do pĂłl struktury: (*th_data).pole
+    //TODO (przy zadaniu 1) klawiatura -> wysyĹanie albo odbieranie -> wyĹwietlanie
+
+    pthread_exit(NULL);
+}
+
+//funkcja obsĹugujÄca poĹÄczenie z nowym klientem
+void handleConnection(int connection_socket_descriptor) {
+    //wynik funkcji tworzÄcej wÄtek
+    int create_result = 0;
+
+    //uchwyt na wÄtek
+    pthread_t thread1;
+
+    //dane, ktĂłre zostanÄ przekazane do wÄtku
+    //TODO dynamiczne utworzenie instancji struktury thread_data_t o nazwie t_data (+ w odpowiednim miejscu zwolnienie pamiÄci)
+    //TODO wypeĹnienie pĂłl struktury
+
+    create_result = pthread_create(&thread1, NULL, ThreadBehavior, (void *)t_data);
+    if (create_result){
+        printf("BĹÄd przy prĂłbie utworzenia wÄtku, kod bĹÄdu: %d\n", create_result);
+        exit(-1);
     }
 
+    //TODO (przy zadaniu 1) odbieranie -> wyĹwietlanie albo klawiatura -> wysyĹanie
 }
 
 int main(int argc, char* argv[])
 {
-    inicjalizacjaSerwera(argv[1], argv[2]);
+    int server_socket_descriptor;
+    int connection_socket_descriptor;
+    int bind_result;
+    int listen_result;
+    char reuse_addr_val = 1;
+    struct sockaddr_in server_address;
 
+    //inicjalizacja gniazda serwera
+
+    memset(&server_address, 0, sizeof(struct sockaddr));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(SERVER_PORT);
+
+    server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_descriptor < 0)
+    {
+        fprintf(stderr, "%s: BĹÄd przy prĂłbie utworzenia gniazda..\n", argv[0]);
+        exit(1);
+    }
+    setsockopt(server_socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse_addr_val, sizeof(reuse_addr_val));
+
+    bind_result = bind(server_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
+    if (bind_result < 0)
+    {
+        fprintf(stderr, "%s: BĹÄd przy prĂłbie dowiÄzania adresu IP i numeru portu do gniazda.\n", argv[0]);
+        exit(1);
+    }
+
+    listen_result = listen(server_socket_descriptor, QUEUE_SIZE);
+    if (listen_result < 0) {
+        fprintf(stderr, "%s: BĹÄd przy prĂłbie ustawienia wielkoĹci kolejki.\n", argv[0]);
+        exit(1);
+    }
+
+    while(1)
+    {
+        connection_socket_descriptor = accept(server_socket_descriptor, NULL, NULL);
+        if (connection_socket_descriptor < 0)
+        {
+            fprintf(stderr, "%s: BĹÄd przy prĂłbie utworzenia gniazda dla poĹÄczenia.\n", argv[0]);
+            exit(1);
+        }
+
+        handleConnection(connection_socket_descriptor);
+    }
+
+    close(server_socket_descriptor);
+    return(0);
 }
